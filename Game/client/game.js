ef32ad113ef31b3c5246aa6f4740781a1adb5243
vchmovzh <vytalyi.chmovzh@hotmail.com>
2014-03-12 17:49:06 +0200

    New tiles, new code

---
+++ Game/client/game.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', onWindowLoad);
+
+function onWindowLoad() {
+    //Start point
+    //Setting up Quintus
+
+    var Q = window.Q = Quintus({development: true})
+        .include('Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio')
+        .setup('game')
+        .controls(true).touch();
+
+    var DIRECTION;
+
+    //Setting up constants
+    DIRECTION = {
+        LEFT: 'left',
+        RIGHT: 'right'
+    };
+
+    Q.SPRITE_PLAYER = 1;
+    Q.SPRITE_GROUND = 2;
+    Q.SPRITE_PLATFORM = 4;
+
+    //Setting up sprites
+    Q.Sprite.extend('Player', {
+        init: function(p) {
+            this._super(p, {
+                name: 'Player1',
+                sheet: 'player',
+                direction: DIRECTION.LEFT,
+                type: Q.SPRITE_PLAYER,
+                collisionMask: Q.SPRITE_DEFAULT | Q.SPRITE_GROUND | Q.SPRITE_PLATFORM
+            });
+
+            this.add('2d, platformerControls');
+        }
+    });
+
+    //Setting up stage
+    Q.scene('level1', function(stage) {
+        Q.stageTMX('level1.tmx', stage);
+        stage.insert(new Q.Player({x: 500, y: 100 }));
+    });
+
+    //Load resources
+    Q.loadTMX('level1.tmx, player.png, player.json', function() {
+        Q.compileSheets('player.png', 'player.json');
+        Q.stageScene('level1');
+    });
+
+}
